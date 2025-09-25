12a0cc80f562beaadb3d5d65a430c55d73663e3e
Khaleel117 <khtsayed@amazon.com>
2025-09-25 17:52:22 +0530

    Update timing-attack.ts

--- timing-attack/timing-attack.ts
+++ timing-attack/timing-attack.ts
@@ -4,7 +4,7 @@ var app = express()
 const password = "myPass";
 function timingAttackNoncompliant() {
   app.get("/user/login", function (req: Request, res: Response) {
-    // Noncompliant: '===' operator is used with sensitive data field.
+    // Noncompliant: '===' operator is used with sensitive data field. 
     if (password === "myPass") {
       // logIn()
     }
